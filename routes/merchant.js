bc0c25eeacce0c036e3dd9c9a92e48c76e201d9a
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-01 05:40:08 +0700

    passed qa for merchant

--- routes/merchant.js
+++ routes/merchant.js
@@ -43,7 +43,7 @@ router.post('/merchant', (req, res) => {
   }
 })
 
-router.delete('/merchant/:merchantId', (req, res) => {
+router.delete('/merchant/:merchantId', async (req, res) => {
   try {
     const { merchantId } = req.params
 
@@ -53,113 +53,26 @@ router.delete('/merchant/:merchantId', (req, res) => {
       return
     }
 
+    // Check if Merchant is Found
+    try {
+      const sqlQuery = `SELECT * FROM Merchant WHERE Merchant_ID='${merchantId}'`
+      const result = await db.promise().query(sqlQuery)
+
+      if (result[0].length === 0) {
+        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+        return
+      }
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
     try {
       const sqlQuery = `DELETE FROM Merchant WHERE Merchant_ID='${merchantId}'`
       db.query(sqlQuery)
       res.status(200).send(response.responseSuccess('200 OK', 'Success Delete Merchant'))
       return
-    } catch (e) {
-      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-      return
-    }
-
-  } catch (e) {
-    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-    return
-  }
-})
-
-router.put('/merchant/:merchantId/:productId', async (req, res) => {
-  try {
-    const { merchantId, productId } = req.params
-    const { name, quantity, price } = req.body
-
-    // Validation
-    if (typeof (name) !== 'string' || typeof (quantity) !== 'number' || typeof (price) !== 'number') {
-      res.status(400).send(response.responseError('400 Bad Request', 'Request Body Not Correct'))
-      return
-    }
-
-    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
-      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
-      return
-    }
-
-    if (productId.length !== 16 || Number.isInteger(merchantId)) {
-      res.status(404).send(response.responseError('404 Not Found', 'Product ID Not Correct'))
-      return
-    }
-
-    // Check is Product Found
-    try {
-      const sqlQuery = `SELECT * FROM Product WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
-      const result = await db.promise().query(sqlQuery)
-
-      if (result[0].length === 0) {
-        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
-        return
-      }
-
-    } catch (e) {
-      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-      return
-    }
-
-    try {
-      const sqlQueryUpdate = `UPDATE Product SET Name='${name}', Quantity='${quantity}', Price='${price}' WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
-      db.query(sqlQueryUpdate)
-
-      const sqlQueryGet = `SELECT Product_ID, Name, Quantity, Price FROM Product WHERE Merchant_ID='${merchantId}' AND Merchant_ID='${merchantId}'`
-      const results = await db.promise().query(sqlQueryGet)
-      res.status(200).send(response.responseSuccess('200 OK', 'Success Get Product', results[0]))
-      return
-
-    } catch (e) {
-      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-      return
-    }
-
-  } catch (e) {
-    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-    return
-  }
-})
-
-router.delete('/merchant/:merchantId/:productId', async (req, res) => {
-  try {
-    const { merchantId, productId } = req.params
-
-    // Validation
-    if (productId.length !== 16 || Number.isInteger(merchantId)) {
-      res.status(404).send(response.responseError('404 Not Found', 'Product ID Not Correct'))
-      return
-    }
-
-    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
-      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
-      return
-    }
-
-    try {
-      const sqlQuery = `SELECT * FROM Product WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
-      const result = await db.promise().query(sqlQuery)
-
-      if (result[0].length === 0) {
-        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
-        return
-      }
-
-    } catch (e) {
-      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-      return
-    }
-
-    try {
-      const sqlQuery = `DELETE FROM Product WHERE Merchant_ID='${merchantId}' AND Merchant_ID='${merchantId}'`
-      db.query(sqlQuery)
-      res.status(200).send(response.responseSuccess('200 OK', 'Success Delete Merchant'))
-      return
-
     } catch (e) {
       res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
       return
--- routes/product.js
+++ routes/product.js
@@ -74,4 +74,106 @@ router.post('/merchant/:merchantId', (req, res) => {
   }
 })
 
+router.put('/merchant/:merchantId/:productId', async (req, res) => {
+  try {
+    const { merchantId, productId } = req.params
+    const { name, quantity, price } = req.body
+
+    // Validation
+    if (typeof (name) !== 'string' || typeof (quantity) !== 'number' || typeof (price) !== 'number') {
+      res.status(400).send(response.responseError('400 Bad Request', 'Request Body Not Correct'))
+      return
+    }
+
+    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+      return
+    }
+
+    if (productId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Product ID Not Correct'))
+      return
+    }
+
+    // Check is Product Found
+    try {
+      const sqlQuery = `SELECT * FROM Product WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
+      const result = await db.promise().query(sqlQuery)
+
+      if (result[0].length === 0) {
+        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+        return
+      }
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+    try {
+      const sqlQueryUpdate = `UPDATE Product SET Name='${name}', Quantity='${quantity}', Price='${price}' WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
+      db.query(sqlQueryUpdate)
+
+      const sqlQueryGet = `SELECT Product_ID, Name, Quantity, Price FROM Product WHERE Merchant_ID='${merchantId}' AND Merchant_ID='${merchantId}'`
+      const results = await db.promise().query(sqlQueryGet)
+      res.status(200).send(response.responseSuccess('200 OK', 'Success Get Product', results[0]))
+      return
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+  } catch (e) {
+    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+    return
+  }
+})
+
+router.delete('/merchant/:merchantId/:productId', async (req, res) => {
+  try {
+    const { merchantId, productId } = req.params
+
+    // Validation
+    if (productId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Product ID Not Correct'))
+      return
+    }
+
+    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+      return
+    }
+
+    try {
+      const sqlQuery = `SELECT * FROM Product WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
+      const result = await db.promise().query(sqlQuery)
+
+      if (result[0].length === 0) {
+        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+        return
+      }
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+    try {
+      const sqlQuery = `DELETE FROM Product WHERE Merchant_ID='${merchantId}' AND Merchant_ID='${merchantId}'`
+      db.query(sqlQuery)
+      res.status(200).send(response.responseSuccess('200 OK', 'Success Delete Merchant'))
+      return
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+  } catch (e) {
+    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+    return
+  }
+})
+
 module.exports = router
