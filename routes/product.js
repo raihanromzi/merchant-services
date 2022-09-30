cf691d833933e393ead62804f5993ea28837e873
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-01 05:44:39 +0700

    add validation when add product by merchant id

--- routes/product.js
+++ routes/product.js
@@ -5,33 +5,6 @@ const { nanoid } = require('nanoid')
 
 const router = express.Router()
 
-
-router.get('/merchant/:merchantId', async (req, res) => {
-  try {
-    const { merchantId } = req.params
-
-    //Validation
-    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
-      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Found'))
-      return
-    }
-
-    try {
-      const sqlQuery = `SELECT Product_ID, Name, Quantity, Price FROM Product WHERE Merchant_ID='${merchantId}'`
-      const results = await db.promise().query(sqlQuery)
-      res.status(200).send(response.responseSuccess('200 OK', 'Success Get Product', results[0]))
-
-    } catch (e) {
-      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-      return
-    }
-
-  } catch (e) {
-    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
-    return
-  }
-})
-
 router.post('/merchant/:merchantId', (req, res) => {
   try {
     const { merchantId } = req.params
@@ -63,6 +36,39 @@ router.post('/merchant/:merchantId', (req, res) => {
         'price': price
       }))
       return
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+  } catch (e) {
+    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+    return
+  }
+})
+
+router.get('/merchant/:merchantId', async (req, res) => {
+  try {
+    const { merchantId } = req.params
+
+    //Validation
+    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Found'))
+      return
+    }
+
+    try {
+      const sqlQuery = `SELECT Product_ID, Name, Quantity, Price FROM Product WHERE Merchant_ID='${merchantId}'`
+      const results = await db.promise().query(sqlQuery)
+
+      if (results[0].length === 0) {
+        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+        return
+      }
+
+      res.status(200).send(response.responseSuccess('200 OK', 'Success Get Product', results[0]))
+      return
+
     } catch (e) {
       res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
       return
