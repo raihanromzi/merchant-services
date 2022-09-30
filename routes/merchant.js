d00369399a1ea82ac689e4540bb2e3930ca8013a
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-01 05:32:24 +0700

    add delete api product

--- routes/merchant.js
+++ routes/merchant.js
@@ -125,4 +125,50 @@ router.put('/merchant/:merchantId/:productId', async (req, res) => {
   }
 })
 
+router.delete('/merchant/:merchantId/:productId', async (req, res) => {
+  try {
+    const { merchantId, productId } = req.params
+
+    // Validation
+    if (productId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Product ID Not Correct'))
+      return
+    }
+
+    if (merchantId.length !== 16 || Number.isInteger(merchantId)) {
+      res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+      return
+    }
+
+    try {
+      const sqlQuery = `SELECT * FROM Product WHERE Product_ID='${productId}' AND Merchant_ID='${merchantId}'`
+      const result = await db.promise().query(sqlQuery)
+
+      if (result[0].length === 0) {
+        res.status(404).send(response.responseError('404 Not Found', 'Merchant ID Not Correct'))
+        return
+      }
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+    try {
+      const sqlQuery = `DELETE FROM Product WHERE Merchant_ID='${merchantId}' AND Merchant_ID='${merchantId}'`
+      db.query(sqlQuery)
+      res.status(200).send(response.responseSuccess('200 OK', 'Success Delete Merchant'))
+      return
+
+    } catch (e) {
+      res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+      return
+    }
+
+  } catch (e) {
+    res.status(500).send(response.responseError('500 Server Error', 'Server Error Please Wait'))
+    return
+  }
+})
+
 module.exports = router
